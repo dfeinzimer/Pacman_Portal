51b68713cab49df4e5ece25b1a8c60c528839197
David Feinzimer <dfeinzimer@gmail.com>
2018-10-05 13:22:26 -0700

    0.0.1

---
+++ game.py
@@ -0,0 +1,33 @@
+import pygame
+from eventloop import EventLoop
+from maze import Maze
+
+
+class Game:
+    BLACK  = (0, 0, 0)
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((680, 740))
+        pygame.display.set_caption("Pacman Portal")
+
+        self.maze = Maze(self.screen, mazefile='pacmap.txt',
+                         brickfile='brick.png', portalfile='brick.png', shieldfile='brick.png', pointfile='brick.png')
+
+    def __str__(self): return 'Game(Pacman Portal), maze=' + str(self.maze) + ')'
+
+    def play(self):
+        eloop = EventLoop(finished=False)
+
+        while not eloop.finished:
+            eloop.check_events()
+            self.update_screen()
+
+    def update_screen(self):
+        self.screen.fill(Game.BLACK)
+        self.maze.blitme()
+        pygame.display.flip()
+
+
+game = Game()
+game.play()
