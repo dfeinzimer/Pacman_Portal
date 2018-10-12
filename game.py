cfd0c9e0ebf7238e76cdd3de399d43604a0a80ff
David Feinzimer <dfeinzimer@gmail.com>
2018-10-12 10:16:22 -0700

    0.1.1

--- game.py
+++ game.py
@@ -8,7 +8,7 @@ class Game:
 
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((680, 740))
+        self.screen = pygame.display.set_mode((460, 510))
         pygame.display.set_caption("Pacman Portal")
 
         self.maze = Maze(self.screen, mazefile='pacmap.txt',
